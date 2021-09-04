f71c9d72ad7710d8fee3ea323d4026b9cbb4014d
SebastianDelgad <delgado.sebastian@correounivalle.edu.co>
2021-09-04 00:37:13 -0500

    Seleccionar archivo PDF

--- App web/src/Components/PagClassifier.jsx
+++ App web/src/Components/PagClassifier.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import UploadPDF from "./UploadPDF"
 
 const PagClassifier = () => {
   return (
@@ -11,6 +12,32 @@ const PagClassifier = () => {
               Asegúrese de que el documento este en formato PDF, una vez cargue
               el documento seleccione un formato para hacer las calificaciones
             </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h4>Archivos a evaluar</h4>
+          </div>
+          <div className="col">
+            <UploadPDF />
+          </div>
+        </div>
+        <br></br>
+        <div className="row">
+          <div className="col">
+            <button className="btn btn-outline-danger">
+              <span> Alfabéticamente </span>
+            </button>
+          </div>
+          <div className="col">
+            <button className="btn btn-outline-danger">
+              <span> Mejor promedio de calificaciones </span>
+            </button>
+          </div>
+          <div className="col">
+            <button className="btn btn-outline-danger">
+              <span> Peor promedio de calificaciones </span>
+            </button>
           </div>
         </div>
       </div>
---
+++ App web/src/Components/UploadPDF.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+
+const UploadPDF = () => {
+
+    const [pdfFile, setPdfFile] = useState(null);
+    const [pdfFileError, setPdfFileError] = useState('');
+
+    const fileType=['application/pdf'];
+    const handlePdfFileChange = (e)=>{
+        let selectedFile = e.target.files[0]
+        if(selectedFile){
+            if(selectedFile&&fileType.includes(selectedFile.type)){
+                let reader = new FileReader();
+                reader.readAsDataURL(selectedFile);
+                reader.onload = (e) =>{
+                    setPdfFile(e.target.result);
+                    setPdfFileError('');
+                }
+            }
+            else{
+                setPdfFile(null);
+                setPdfFileError('Please select valid file')
+            }
+
+        }
+        else{
+            console.log('select your file')
+        }
+    }
+
+    const handlePdfFileSubmit = (e) => {
+        e.preventDefault();
+        if(pdfFile!==null){
+
+        }
+    }
+
+    return ( 
+        <div>
+            <form className="form-group" onSubmit={handlePdfFileSubmit}>
+            <input type="file" className="form-control" 
+            onChange={handlePdfFileChange} />
+            {pdfFileError&& <div className="alert alert-danger">{pdfFileError}</div>}
+            </form>
+        </div>
+     );
+}
+ 
+export default UploadPDF;
