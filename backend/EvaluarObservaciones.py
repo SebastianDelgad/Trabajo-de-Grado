9d243c93e8a80f07da572cdcfc1ad8c3cb349921
SebastianDelgad <delgado.sebastian@correounivalle.edu.co>
2021-10-08 01:22:16 -0500

    fix

--- backend/EvaluarObservaciones.py
+++ backend/EvaluarObservaciones.py
@@ -1,6 +1,6 @@
 import firebase_admin.storage
 
-from LeerObservaciones import  almacenar_nombres
+from LeerObservaciones import almacenar_nombres
 from Preprocesado import dataset
 import numpy as np
 import os
@@ -8,7 +8,7 @@ from firebase_admin import credentials, initialize_app, storage, db
 
 # Fetch the service account key JSON file contents
 module_dir = os.path.dirname(__file__)
-credential = os.path.join(module_dir, 'teacher-qualifier-firebase-adminsdk-33adn-c47f32bda8.json')
+credential = os.path.join(module_dir, 'teacher-qualifier-firebase-adminsdk-rcl5m-f2037fdc9f.json')
 
 cred = credentials.Certificate(credential)
 
@@ -16,18 +16,17 @@ initialize_app(cred, {
     'storageBucket': 'teacher-qualifier.appspot.com',
 })
 
-def almacenar():
 # Put your local file path 
-    fileName = "resultado1.txt"
-    file = os.path.join(module_dir, fileName)
-    bucket = storage.bucket()
-    blob = bucket.blob(fileName)
-    blob.upload_from_filename(file)
+fileName = "resultado1.txt"
+file = os.path.join(module_dir, fileName)
+bucket = storage.bucket()
+blob = bucket.blob(fileName)
+blob.upload_from_filename(file)
 
 # Opt : if you want to make public access from the URL
-    blob.make_public()
+blob.make_public()
 
-    print("your file url", blob.public_url)
+print("your file url", blob.public_url)
 
 
 def evaluar_documento(txt):
