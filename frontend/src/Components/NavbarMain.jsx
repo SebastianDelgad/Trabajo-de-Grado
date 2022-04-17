17c566b8dc02a9cbc11a37a064ae48bc4c5b33da
SebastianDelgad <delgado.sebastian@correounivalle.edu.co>
2022-04-17 16:59:25 -0500

    logout every 4 hours

--- frontend/src/Components/NavbarMain.jsx
+++ frontend/src/Components/NavbarMain.jsx
@@ -12,11 +12,45 @@ export const NavbarMain = () => {
     auth.onAuthStateChanged((user) => {
       if (user) {
         setFirebaseUser(user);
+        logout(user);
       } else {
         setFirebaseUser(null);
       }
     });
   }, []);
+
+  function logout(user) {
+    const fechaLogin = user.metadata.lastSignInTime.replace("GMT", "");
+    const diaLogin = fechaLogin[5] + fechaLogin[6];
+    const horaLogin = fechaLogin[17] + fechaLogin[18];
+
+    const fechaLog = new Date();
+    var options = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    options.timeZone = "GMT";
+    options.timeZoneName = "short";
+
+    const FechaActual = fechaLog
+      .toLocaleString("en-GB", options)
+      .replace("UTC", "");
+    const diaActual = FechaActual[5] + FechaActual[6];
+    const horaActual = FechaActual[18] + FechaActual[19];
+
+    if (diaLogin === diaActual) {
+      if (parseInt(horaActual) - parseInt(horaLogin) >= 4) {
+        auth.signOut();
+      }
+    } else {
+      auth.signOut();
+    }
+  }
 
   function handleClickIniciarsesion() {
     firebaseUser ? history.push("/evaluaciones") : history.push("/login");
@@ -27,12 +61,12 @@ export const NavbarMain = () => {
       <div className="container mt-3 bg-light rounded-6">
         <div className="row">
           <div className="mt-3 mb-4 col-8 col-sm-9 col-xs-9 col-md-9 col-lg-9 col-xl-9 col-xxl-9">
-          <div className="row">
-          <div className="col-3 col-sm-3 col-xs-2 col-md-2 col-lg-2 col-xl-1 col-xxl-1">
-            <img src={logo_imagen} alt="logoLogin"/>
-            </div>
-            <div className="mt-3 col-5">
-              <strong className="text-danger ">SISCOD</strong>
+            <div className="row">
+              <div className="col-3 col-sm-3 col-xs-2 col-md-2 col-lg-2 col-xl-1 col-xxl-1">
+                <img src={logo_imagen} alt="logoLogin" />
+              </div>
+              <div className="mt-3 col-5">
+                <strong className="text-danger ">SISCOD</strong>
               </div>
             </div>
           </div>
